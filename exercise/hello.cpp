7a47976a462e2ff6f00470ffda6b09bcd38d3c56
MClongxue <1071754593@qq.com>
2021-01-14 18:55:40 +0800

    master

---
+++ exercise/hello.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    printf("hello world!\n");
+    printf("What is you name?");
+    char name[10];
+    scanf("%s",name);
+    printf("hello %s ,I am Xiaoai. Nice to meet you!\n",name);
+
+    //return 0;
+}
